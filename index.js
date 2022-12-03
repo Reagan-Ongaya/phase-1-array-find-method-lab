dd25a6b03a4005cd9284714936b412a1e84b263e
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-12-03 22:41:46 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+];
+
+function superbolWin(time){
+    return time.year === "2015";
+}
+
+console.log(record.find(is2015));
